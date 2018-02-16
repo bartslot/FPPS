2a33f62513d2a769b2818923fa0a7f6fc619327f
bartslot <bartslot@gmail.om>
2018-02-16 13:37:39 +0100

    Versie met Tempel

---
+++ Untitled/Assets/Helicopter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helicopter : MonoBehaviour {
+ 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		transform.position = new Vector3 (transform.position.x, transform.position.y + .1f, transform.position.z);
+		transform.eulerAngles = new Vector3 (transform.eulerAngles.x, transform.eulerAngles.y + 10f, transform.eulerAngles.z);
+	}
+}
